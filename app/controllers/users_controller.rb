d941dbe8e00bfa48b2dce3849e4f77a4ee5e269d
Donatien Rolland <donatien@rollandmail.com>
2017-11-27 18:59:41 +0100

    first step dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def dashboard
+    @user = User.find(params[:id])
+    @bikes = @user.bikes
+
+    @reservations = Reservation.where(user: @user)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.new(first_name: "Bob", last_name: "Marcel", email: "bob.marcel@gmail.com")
+donatien = User.find(1)
 
-bike1 = Bike.new(title: "Nice racing bike", description: "Very fast and light", price_per_day: 15, photo: "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg", address: "Paris")
-bike2 = Bike.new(title: "Nice bike", description: "Very fast and light", price_per_day: 20, photo: "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg", address: "Paris")
-bike3 = Bike.new(title: "Racing bike", description: "Very fast and light", price_per_day: 12, photo: "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg", address: "Paris")
-bike4 = Bike.new(title: "Very Nice racing bike", description: "Very fast and light", price_per_day: 25, photo: "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg", address: "Paris")
+bike1 = Bike.new(title: "Nice racing bike", description: "Very fast and light", price_per_day: 15, address: "Paris")
+bike2 = Bike.new(title: "Nice bike", description: "Very fast and light", price_per_day: 20, address: "Paris")
+bike3 = Bike.new(title: "Racing bike", description: "Very fast and light", price_per_day: 12, address: "Paris")
+bike4 = Bike.new(title: "Very Nice racing bike", description: "Very fast and light", price_per_day: 25, address: "Paris")
 
-bike1.user = user1
-bike2.user = user1
-bike3.user = user1
-bike4.user = user1
+bike1.user = donatien
+bike2.user = donatien
+bike3.user = donatien
+bike4.user = donatien
+
+
+
+bike1.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
+bike2.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
+bike3.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
+bike4.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
+
+bike1.save!
+bike2.save!
+bike3.save!
+bike4.save!
