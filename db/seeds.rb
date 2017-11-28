e9788484032db9ecb0d7e31a94c813c59416dc0d
Donatien Rolland <donatien@rollandmail.com>
2017-11-28 16:12:17 +0100

    almost good

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-donatien = User.find(1)
+# donatien = User.find(1)
 
-bike1 = Bike.new(title: "Nice racing bike", description: "Very fast and light", price_per_day: 15, address: "Paris")
-bike2 = Bike.new(title: "Nice bike", description: "Very fast and light", price_per_day: 20, address: "Paris")
-bike3 = Bike.new(title: "Racing bike", description: "Very fast and light", price_per_day: 12, address: "Paris")
-bike4 = Bike.new(title: "Very Nice racing bike", description: "Very fast and light", price_per_day: 25, address: "Paris")
+# bike1 = Bike.new(title: "Nice racing bike", description: "Very fast and light", price_per_day: 15, address: "Paris")
+# bike2 = Bike.new(title: "Nice bike", description: "Very fast and light", price_per_day: 20, address: "Paris")
+# bike3 = Bike.new(title: "Racing bike", description: "Very fast and light", price_per_day: 12, address: "Paris")
+# bike4 = Bike.new(title: "Very Nice racing bike", description: "Very fast and light", price_per_day: 25, address: "Paris")
 
-bike1.user = donatien
-bike2.user = donatien
-bike3.user = donatien
-bike4.user = donatien
+# bike1.user = donatien
+# bike2.user = donatien
+# bike3.user = donatien
+# bike4.user = donatien
 
 
 
-bike1.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
-bike2.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
-bike3.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
-bike4.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
+# bike1.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
+# bike2.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
+# bike3.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
+# bike4.remote_photo_url = "https://www.bricklanebikes.co.uk/content/images/thumbs/0030350_6ku-fixie-single-speed-bike-beach-bum_1200.jpeg"
 
-bike1.save!
-bike2.save!
-bike3.save!
-bike4.save!
+# bike1.save!
+# bike2.save!
+# bike3.save!
+# bike4.save!
+
+
+Reservation.create(user_id: 2, bike_id: 1, starts_at: DateTime.new(2017,12,2), ends_at: DateTime.new(2017,12,4))
+Reservation.create(user_id: 2, bike_id: 2, starts_at: DateTime.new(2017,12,5), ends_at: DateTime.new(2017,12,7))
