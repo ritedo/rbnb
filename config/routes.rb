ffd2e7cb2006d0c56ae8292122e71f3584f33f28
marguerite dorangeon <margueritedorangeon@hotmail.fr>
2017-11-28 16:01:27 +0100

    new mybikes page

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :bikes, only: [:index, :show, :new, :create] do
+  resources :bikes, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :reservations, only: [:new, :create, :index]
+    collection do
+      get 'mybikes', to: "bikes#mybikes", as: :mybikes
+    end
   end
   get 'users/:id/dashboard', to: "users#dashboard", as: :dashboard
 end
