d2d0af29df81a0cb6a2c4eefbba42196bb40f50c
Donatien Rolland <donatien@rollandmail.com>
2017-11-28 17:24:51 +0100

    seed is ok

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,6 +1,6 @@
 class Bike < ApplicationRecord
   belongs_to :user
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   validates :title, :description, :photo, :price_per_day, :address, presence: true
   mount_uploader :photo, PhotoUploader
 end
