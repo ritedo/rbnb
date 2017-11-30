f940adab0b1e152b03430c4d66e10ff222545206
marguerite dorangeon <margueritedorangeon@hotmail.fr>
2017-11-30 19:14:23 +0100

    added validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130101942) do
+ActiveRecord::Schema.define(version: 20171130165516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20171130101942) do
     t.bigint "bike_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "validated"
     t.index ["bike_id"], name: "index_reservations_on_bike_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
